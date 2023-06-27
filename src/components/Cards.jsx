7bad898082a0f4d42e496dc18fb149c0d3b9f25d
juniorgutierrez05 <junior.gutierrezm05@gmail.com>
2023-06-27 08:33:43 -0500

    1

---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+
+export default function Cards(props) {
+   const {characters} = props
+   return <div>
+      {characters.map((character) => (
+        <Card
+          key={character.id}
+          name={character.name}
+          status={character.status}
+          species={character.species}
+          gender={character.gender}
+          origin={character.origin.name}
+          image={character.image}
+          onClose={() => window.alert("Emulacion")}
+        />
+      ))}
+   </div>;
+}
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+export default function SearchBar(props) {
+ 
+
+  return (
+    <div>
+      <input
+        type="search"
+      />
+      <button onClick={props.onSearch}>Agregar</button>
+    </div>
+  );
+}
+
